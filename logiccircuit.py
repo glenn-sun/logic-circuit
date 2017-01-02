8000db8c250555f9fec6dd8d25361c9b6a06ce28
Glenn Sun <Glenn Sun>
2017-01-02 18:47:29 -0500

    Add basic partial eval functionality

--- logiccircuit.py
+++ logiccircuit.py
@@ -10,10 +10,30 @@ class ConstTrue(Node):
 	def __init__(self):
 		super(ConstTrue, self).__init__()
 
+	def eval(self):
+		return True
+
 class ConstFalse(Node):
 	def __init__(self):
 		super(ConstFalse, self).__init__()
 
+	def eval(self):
+		return False
+
 class And(Node):
 	def __init__(self, *args):
 		super(And, self).__init__(*args)
+
+	def eval(self):
+		result = True
+		for node in graph[self]:
+			result = result and node.eval()
+		return result
+
+# Evalute for a certain variable
+def partial_eval(node, return_result=False):
+	result = node.eval()
+	if return_result:
+		return result
+	else:
+		print result
